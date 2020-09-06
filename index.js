df33840255ab8cb16dda0bafeb749f68f6aa0f80
Ricardo <rdobmk@gmail.com>
2020-09-06 16:42:58 +0200

    end of fase 1

--- index.js
+++ index.js
@@ -67,8 +67,9 @@ const inputFunc = async (event) =>{
         options.addEventListener('click', () => {
             theDropdown.classList.remove('is-active');
             inputMovie.value = movie.Title;
+            fetchMovieSelected(movie);
         });
-        
+
         resultWrapper.appendChild(options);
     }
 }
@@ -80,4 +81,77 @@ document.addEventListener('click', (event) => {
     if (!root.contains(event.target)) {
         theDropdown.classList.remove('is-active');
     }
-})
+});
+
+const fetchMovieSelected = async (movie) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'b262c28d', 
+            i: movie.imdbID
+        }
+    });
+    console.log(response.data);
+    const movieDetail = response.data;
+    document.querySelector('#movieinfo').innerHTML = movieInformation(movieDetail);
+}
+
+const movieInformation = (movieDetail) => {
+    return `
+        <article class="media">
+            <figure class="media-left">
+                <p class="image">
+                    <img src="${movieDetail.Poster}"/>
+                </p>
+            </figure>
+            <div class="media-content">
+                <div class="content">
+                    <h1>${movieDetail.Title}</h1>
+                    <h4>${movieDetail.Genre}</h4>
+                    <p>${movieDetail.Plot}</p>
+                </div>
+            </div>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Actors}</p>
+            <p class="subtitle">Actors</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Director}</p>
+            <p class="subtitle">Director</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Ratings</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Ratings[0].Source}: ${movieDetail.Ratings[0].Value} </p>
+            <p class="title">${movieDetail.Ratings[1].Source}: ${movieDetail.Ratings[1].Value} </p>
+            <p class="title">${movieDetail.Ratings[2].Source}: ${movieDetail.Ratings[2].Value} </p>
+            <p class="subtitle">Ratings</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Released}</p>
+            <p class="subtitle">Released date</p>
+        </article>
+        <article class="notification is-primary">
+            <p class="title">${movieDetail.Runtime}</p>
+            <p class="subtitle">Runtime</p>
+        </article>
+    `;
+}
