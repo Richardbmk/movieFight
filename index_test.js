42f148f79049efabb92ddedbf501a6a25608514c
Ricardo <rdobmk@gmail.com>
2020-09-07 21:48:51 +0200

    testing only

--- index_test.js
+++ index_test.js
@@ -1,54 +1,26 @@
 const fetchMovies = async (searchTerm) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
-            apikey: "b262c28d",
+            apikey: 'b262c28d',
             s: searchTerm
         }
     });
-    if(response.data.Error){
+    if (response.Error) {
         return [];
     }
-    return response.data.Search;
+    return response.data;
 }
 
-const input = document.querySelector('input');
+const inputMovie = document.querySelector('input');
 
-const bounceHelper = (func, delay = 1000) => {
-    let timingID;
-    return (...args) => {
-        if (timingID) {
-            clearInterval(timingID)
-        }
-        timingID = setTimeout(() => {
-            func.apply(null, args);
-        }, delay)
+let timingID;
+const inputFunc = (event) => {
+    if(timingID) {
+        clearInterval(timingID);
     }
+    timingID = setTimeout(() => {
+        fetchMovies(event.target.value);
+    }, 1000);
 }
 
-
-
-const inputCalback = async (event) => {
-    const movies = await fetchMovies(event.target.value);
-    // console.log(movies)
-
-    for (let movie of movies) {
-        const div = document.createElement('div');
-
-        div.innerHTML = `
-            <img src="${movie.Poster}" />
-            <h1>${movie.Title}</h1>
-        `;
-
-        document.querySelector('#target').appendChild(div);
-    }
-
-
-}
-
-input.addEventListener('input', bounceHelper(inputCalback, 500));
-
-
-
-
-
-
+inputMovie.addEventListener('input', inputFunc)
