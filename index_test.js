4b207a7aac4cd9582b35357548941fd8b474ce1f
Ricardo <rdobmk@gmail.com>
2020-09-08 21:33:07 +0200

    only testing

--- index_test.js
+++ index_test.js
@@ -5,22 +5,76 @@ const fetchMovies = async (searchTerm) => {
             s: searchTerm
         }
     });
-    if (response.Error) {
+    if(response.data.Error) {
         return [];
     }
-    return response.data;
+
+    return response.data.Search;
 }
 
-const inputMovie = document.querySelector('input');
+const root = document.querySelector('#autocomplete');
 
-let timingID;
-const inputFunc = (event) => {
-    if(timingID) {
-        clearInterval(timingID);
+root.innerHTML = `
+    <label><b>Search for a Movie</b></label>
+    <input class="input"/>
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dowpdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const theDropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+
+const bouceHelper = (func, delay = 1000) => {
+    let timingID;
+    return (...args) => {
+        if(timingID) {
+            clearInterval(timingID);
+        }
+        timingID = setTimeout(() => {
+            func.apply(null, args);
+        }, delay)
     }
-    timingID = setTimeout(() => {
-        fetchMovies(event.target.value);
-    }, 1000);
 }
 
-inputMovie.addEventListener('input', inputFunc)
+
+const inputFunc = async (event) => {
+    const movies = await fetchMovies(event.target.value);
+
+    if(!movies.length) {
+        theDropdown.classList.remove('is-active');
+        return;
+    }
+
+    resultsWrapper.innerHTML = '';
+    theDropdown.classList.add('is-active');
+
+    for (let movie of movies) {
+        const options = document.createElement('a');
+        options.classList.add('dropdown-item');
+        const imgsrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+        options.innerHTML = `
+            <img src="${imgsrc}"/>
+            <h2>${movie.Title}</h2>
+        `;
+        options.addEventListener('click', () => {
+            theDropdown.classList.remove('is-active');
+            input.value = movie.Title;
+        })
+
+
+        resultsWrapper.appendChild(options);
+    }
+}
+
+input.addEventListener('input', bouceHelper(inputFunc, 500));
+
+document.addEventListener('click', (event) => {
+    if(!root.contains(event.target)) {
+        theDropdown.classList.remove('is-active');
+    }
+});
